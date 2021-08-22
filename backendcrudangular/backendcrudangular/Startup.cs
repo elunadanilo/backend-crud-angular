95c93ab98a5efe0c5e5c8c6ef9978259e55cd438
elunadanilo <eluna.danilo@gmail.com>
2021-08-22 00:16:26 -0600

    cors
    Se agrega cors

--- backendcrudangular/backendcrudangular/Startup.cs
+++ backendcrudangular/backendcrudangular/Startup.cs
@@ -36,6 +36,11 @@ namespace backendcrudangular
 
             services.AddDbContext<AplicationDbContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+            services.AddCors(options => options.AddPolicy("AllowWebApp",
+                builder => builder.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod()));  
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +52,8 @@ namespace backendcrudangular
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "backendcrudangular v1"));
             }
+
+            app.UseCors("AllowWebApp");
 
             app.UseHttpsRedirection();
 
