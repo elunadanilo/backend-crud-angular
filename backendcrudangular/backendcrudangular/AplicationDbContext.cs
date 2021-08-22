5d1dc9c39513b70bfe9782a26ebefede2eb721da
elunadanilo <eluna.danilo@gmail.com>
2021-08-21 22:07:58 -0600

    Metodos
    Se agregan los metodos necesarios en el api

---
+++ backendcrudangular/backendcrudangular/AplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using backendcrudangular.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backendcrudangular
+{
+    public class AplicationDbContext: DbContext
+    {
+        public DbSet<Tarjeta> TarjetaCredito { get; set; }
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options) 
+        { 
+        
+        }
+    }
+}
---
+++ backendcrudangular/backendcrudangular/Models/Tarjeta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backendcrudangular.Models
+{
+    public class Tarjeta
+    {
+        
+        public int Id { get; set; }
+
+        [Required]
+        public string Titulo { get; set; }
+
+        [Required]
+        public string NumeroTarjeta { get; set; }
+
+        [Required]
+        public string FechaExpiracion { get; set; }
+
+        [Required]
+        public string CVV { get; set; }
+
+
+    }
+}
